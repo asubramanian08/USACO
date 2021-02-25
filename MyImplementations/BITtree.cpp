791426f7d65536e10c5db0596e5e7112ee5c210b
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2021-02-25 18:06:44 -0500

    Adding week 7 starlegue problems

--- MyImplementations/BITtree.cpp
+++ MyImplementations/BITtree.cpp
@@ -1,28 +1,28 @@
-#include <stdlib.h>
+using namespace std;
 
-unsigned int *bTree;
-unsigned int elements;
-void initNoArr(unsigned int elem)
+int *bTree;
+int elements;
+void initNoArr(int elem)
 {
     elements = elem;
-    bTree = (unsigned int *)malloc(sizeof(unsigned int) * elem);
+    bTree = new int[elem];
 }
-void initArr(unsigned int elem, unsigned int *initialize, unsigned int numInit)
+void initArr(int elem, int *initialize, int numInit)
 {
     elements = elem;
-    bTree = (unsigned int *)malloc(sizeof(unsigned int) * elem);
-    for (unsigned int i = 0; i < numInit; i++)
+    bTree = new int[elem];
+    for (int i = 0; i < numInit; i++)
         updt(i, initialize[i]);
 }
-unsigned int query(unsigned int node)
+int query(int node)
 {
-    unsigned int sum = 0;
-    for (unsigned int i = node - 1; i >= 0; i -= (i & -i))
+    int sum = 0;
+    for (int i = node - 1; i >= 0; i -= (i & -i))
         sum += bTree[i];
     return sum;
 }
-void updt(unsigned int node, unsigned int val)
+void updt(int node, int val)
 {
-    for (unsigned int i = node - 1; i < elements; i += (i & -i))
+    for (int i = node - 1; i < elements; i += (i & -i))
         bTree[i] += val;
 }
