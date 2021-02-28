77a9e9c60b9f2089d13ebc7a2fdf24d3a96f5e84
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2021-02-27 20:36:31 -0500

    fix BIT tree

--- MyImplementations/BITtree.cpp
+++ MyImplementations/BITtree.cpp
@@ -1,27 +1,36 @@
-using namespace std;
+class BIT
+{
+private:
+    int *bTree;
+    int elements;
 
-int *bTree;
-int elements;
-void initNoArr(int elem)
+public:
+    BIT(int elem);
+    BIT(int elem, int *initialize, int numInit);
+    ~BIT() { delete[] bTree; }
+    int query(int node);
+    void updt(int node, int val);
+};
+BIT::BIT(int elem)
 {
     elements = elem;
     bTree = new int[elem];
 }
-void initArr(int elem, int *initialize, int numInit)
+BIT::BIT(int elem, int *initialize, int numInit)
 {
     elements = elem;
     bTree = new int[elem];
     for (int i = 0; i < numInit; i++)
         updt(i, initialize[i]);
 }
-int query(int node)
+int BIT::query(int node)
 {
     int sum = 0;
     for (int i = node - 1; i >= 0; i -= (i & -i))
         sum += bTree[i];
     return sum;
 }
-void updt(int node, int val)
+void BIT::updt(int node, int val)
 {
     for (int i = node - 1; i < elements; i += (i & -i))
         bTree[i] += val;
