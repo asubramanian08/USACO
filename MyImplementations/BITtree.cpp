f185fd8bf6c3e791d7c1ad840843136cab90b48b
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2021-02-27 21:32:44 -0500

    improve BIT tree

--- MyImplementations/BITtree.cpp
+++ MyImplementations/BITtree.cpp
@@ -9,29 +9,33 @@ public:
     BIT(int elem, int *initialize, int numInit);
     ~BIT() { delete[] bTree; }
     int query(int node);
-    void updt(int node, int val);
+    void updt(int node, int cng);
 };
 BIT::BIT(int elem)
 {
     elements = elem;
-    bTree = new int[elem];
+    bTree = new int[elem + 1];
+    for (int i = 1; i <= elem; i++)
+        bTree[i] = 0;
 }
 BIT::BIT(int elem, int *initialize, int numInit)
 {
     elements = elem;
-    bTree = new int[elem];
+    bTree = new int[elem + 1];
+    for (int i = 1; i <= elem; i++)
+        bTree[i] = 0;
     for (int i = 0; i < numInit; i++)
         updt(i, initialize[i]);
 }
 int BIT::query(int node)
 {
     int sum = 0;
-    for (int i = node - 1; i >= 0; i -= (i & -i))
+    for (int i = node + 1; i > 0; i -= (i & -i))
         sum += bTree[i];
     return sum;
 }
-void BIT::updt(int node, int val)
+void BIT::updt(int node, int cng)
 {
-    for (int i = node - 1; i < elements; i += (i & -i))
-        bTree[i] += val;
+    for (int i = node + 1; i <= elements; i += (i & -i))
+        bTree[i] += cng;
 }
