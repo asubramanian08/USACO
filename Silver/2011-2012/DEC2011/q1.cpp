7c85a960962e04bb941579fae15daec62deae8a8
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2021-04-17 16:09:37 -0400

    fix DEC2011 silver q1

--- Silver/2011-2012/DEC2011/q1.cpp
+++ Silver/2011-2012/DEC2011/q1.cpp
@@ -1,30 +1,25 @@
 // Start: 9:25
-// Think: 9:35, 11:59-12:17, 12:23-
+// Think: 9:35, 11:59-12:17, 12:23- ~1:00
 // Write: 12:23
 // Debug:
+
+//after looking at solution: 2:30-2:46
 #include <iostream>
 #include <map>
+#include <algorithm>
 using namespace std;
 
-int numCows;
-int *ordering[5];
-int *key;
-
-void idChange(void)
+#define numPictures 5
+map<int, int> pos[numPictures];
+bool cmpPos(int cow1, int cow2)
 {
-    //make ids and key
-    map<int, int> ids;
-    key = new int[numCows];
-    for (int i = 0; i < numCows; i++)
-    {
-        ids[ordering[0][i]] = i;
-        key[i] = ordering[0][i];
-    }
-
-    //change stuff
-    for (int i = 0; i < 5; i++)
-        for (int j = 0; j < numCows; j++)
-            ordering[i][j] = ids[ordering[i][j]];
+    int before = 0, after = 0;
+    for (int i = 0; i < numPictures; i++)
+        if (pos[i][cow1] < pos[i][cow2])
+            before++;
+        else
+            after++;
+    return before > after;
 }
 
 int main(void)
@@ -34,16 +29,21 @@ int main(void)
     freopen("photo.in", "r", stdin);
     freopen("photo.out", "w", stdout);
 #endif
+    int numCows, cow;
+    int *ordering = new int[numCows];
     cin >> numCows;
-    for (int i = 0; i < 5; i++)
-    {
-        ordering[i] = new int[numCows];
+    for (int i = 0; i < numPictures; i++)
         for (int j = 0; j < numCows; j++)
-            cin >> ordering[i][j];
-    }
+        {
+            cin >> cow;
+            pos[i][cow] = j;
+            ordering[j] = cow;
+        }
 
-    //change values
-    idChange();
-
+    //get the ordering, delete and print
+    sort(&ordering[0], &ordering[numCows], cmpPos);
+    for (int i = 0; i < numCows; i++)
+        cout << ordering[i] << endl;
+    delete[] ordering;
     return 0;
 }
