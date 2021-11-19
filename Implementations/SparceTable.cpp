8698978cf37c6b65461ed6c7b205a254c5eed88f
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2021-11-19 10:05:50 -0500

    Fix sparce table implementation

--- Implementations/SparceTable.cpp
+++ Implementations/SparceTable.cpp
@@ -1,10 +1,31 @@
-//star league Part 2 week 7 q1
-#include <iostream>
+// star league Part 2 week 7 q1
+// both min and max in range
 #include <utility>
-#include <algorithm>
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+#define rangeMin(start, end, log) min(st[start][log].first, st[end - (1 << log) + 1][log].first)
+#define rangeMax(start, end, log) max(st[start][log].second, st[end - (1 << log) + 1][log].second)
+#define rangeDif(start, end, log) (rangeMax(start, end, log) - rangeMin(start, end, log))
+#define queryLog(start, end) log2(end - start + 1)
+
+using pii = pair<int, int>;
+
 int main(void)
 {
-    int log, numCows; //these values will already be here
+    // start ST
+    int numCows;
+    pii **st = new pii *[numCows];
+    int log = log2(numCows) + 1;
+    for (int i = 0; i < numCows; i++)
+    {
+        st[i] = new pii[log];
+        cin >> st[i][0].first;
+        st[i][0].second = st[i][0].first;
+    }
+
+    int numCows, log = log2(numCows) + 1;
     pair<int, int> st[100][100];
     for (int j = 1; j < log; j++)
         for (int i = 0; i + (1 << j) <= numCows; i++)
