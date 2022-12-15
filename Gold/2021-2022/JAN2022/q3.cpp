9ed07fe9aced1aff167d6c21755c7ccec0501b87
asubramanian08 <arjun.subramanian.08@gmail.com>
2022-12-14 19:41:57 -0500

    USACO-G-JAN2022 q3 practice

--- Gold/2021-2022/JAN2022/q3.cpp
+++ Gold/2021-2022/JAN2022/q3.cpp
@@ -1,10 +1,38 @@
+// Unofficial: 7:00 - 7:40
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
+
+long long n, arr[100010], value[100010], extra;
+vector<int> *graph;
+int findHeight(int node)
+{
+    int height = 0;
+    for (int next : graph[node])
+        height = max(height, findHeight(next) + 1);
+    return height;
+}
+void DFS(int node, long long height)
+{
+    value[node] = height * (n + 1) + extra--;
+    sort(graph[node].begin(), graph[node].end(), greater<int>());
+    for (int next : graph[node])
+        DFS(next, height - 1);
+}
 
 int main(void)
 {
-    // read
-
-    // print and delete
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    graph = new vector<int>[n + 1];
+    for (int i = 0; i < n; i++)
+        graph[arr[i]].push_back(i);
+    cout << n + 1 << endl; // k = n + 1
+    extra = n;
+    DFS(n, findHeight(n));
+    for (int i = 0; i < n; i++)
+        cout << value[i] << endl;
     return 0;
 }
